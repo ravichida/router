d03d1a9f46cef7e4508522f0c042b2f7880ecf1e
ravi kumar chida <ravi.chanti@gmail.com>
2019-12-20 21:26:02 +0530

    Updates

---
+++ precache-manifest.27168518922611aca00e0a71d9a4373c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5706239331ce73bb004a9f78f9a5c1f",
+    "url": "/router/index.html"
+  },
+  {
+    "revision": "df93ee843ed07e9630ce",
+    "url": "/router/static/css/main.db9cf7ec.chunk.css"
+  },
+  {
+    "revision": "edb1713705985bd95522",
+    "url": "/router/static/js/2.c6bb8c1b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/router/static/js/2.c6bb8c1b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df93ee843ed07e9630ce",
+    "url": "/router/static/js/main.9c61cfde.chunk.js"
+  },
+  {
+    "revision": "c7b786ab5a52fd975043",
+    "url": "/router/static/js/runtime-main.18d1b2c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/router/static/media/logo.5d5d9eef.svg"
+  }
+]);
